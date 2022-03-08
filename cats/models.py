54ffe1038daaec04b763aebf3b221b04affbc21e
Kuzin Sergey <kuzin-sergey@yandex.ru>
2022-03-08 21:26:03 +0300

    sprint7, lesson 9

--- cats/models.py
+++ cats/models.py
@@ -1,11 +1,44 @@
 from django.db import models
 
 
+class Achievement(models.Model):
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.name
+
+
+class Owner(models.Model):
+    first_name = models.CharField(max_length=128)
+    last_name = models.CharField(max_length=128)
+
+    # def __repr__(self):
+    #     return (f'{self.__class__.__name__}('
+    #             f'{self.first_name!r}, {self.last_name!r})')
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+
 class Cat(models.Model):
     name = models.CharField(max_length=16)
     color = models.CharField(max_length=16)
     birth_year = models.IntegerField()
-
+    owner = models.ForeignKey(
+        Owner,
+        on_delete=models.CASCADE,
+        related_name='cats',
+    )
+    achievements = models.ManyToManyField(
+        Achievement, through='AchievementCat')
 
     def __str__(self):
         return self.name
+
+
+class AchievementCat(models.Model):
+    achievement = models.ForeignKey(Achievement, on_delete=models.CASCADE)
+    cat = models.ForeignKey(Cat, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.achievement} {self.cat}'
